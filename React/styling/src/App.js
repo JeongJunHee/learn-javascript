8091c69d2c6965bf805560992b1ca76bdd4e2f3d
JeongJunHee <unizet1@gmail.com>
2018-05-10 16:44:37 +0900

    add styling

--- React/styling/src/App.js
+++ React/styling/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
+import StyledButton from './components/StyledButton';
 
 class App extends Component {
   render() {
+    const isBlue = true;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Button>버튼</Button>
+        <StyledButton big>버튼</StyledButton>
       </div>
     );
   }
