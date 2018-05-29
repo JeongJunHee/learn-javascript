b77df21cf081d56cbb9b0f0e644483ecb5b09169
JeongJunHee <unizet1@gmail.com>
2018-05-29 10:39:51 +0900

    add middleware

---
+++ React/redux-starter-kit/src/modules/post.js
@@ -0,0 +1,35 @@
+import { createAction, handleActions } from 'redux-actions';
+import { pender } from 'redux-pender';
+import axios from 'axios';
+
+function getPostAPI(postId) {
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+}
+
+const GET_POST = 'GET_POST';
+
+export const getPost = createAction(GET_POST, getPostAPI);
+
+const initialState = {
+  data: {
+    title: '',
+    body: ''
+  }
+}
+
+export default handleActions({
+  ...pender({
+    type : GET_POST,
+    // onPending : (state, action) => state,
+    // onFailure : (state, action) => state
+    onSuccess : (state, action) => {
+      const { title, body } = action.payload.data;
+
+      return {
+        data : {
+          title, body
+        }
+      }
+    }
+  })
+}, initialState);
---
+++ React/redux-starter-kit/src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import modules from './modules';
+
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import penderMiddleware from 'redux-pender';
+
+const logger = createLogger(); 
+
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, penderMiddleware()))
+
+export default store;
