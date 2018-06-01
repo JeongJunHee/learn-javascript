add33523f051fc888266e5214714271e08d630a4
JeongJunHee <unizet1@gmail.com>
2018-06-01 10:33:22 +0900

    add react-hot-loader

--- React/react-router-tutorial/src/index.js
+++ React/react-router-tutorial/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './client/Root';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
 import './index.css';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Root />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(Root)
+
+if (module.hot) {
+  module.hot.accept('./client/Root', () => { render(Root) })
+}
+
 registerServiceWorker();
